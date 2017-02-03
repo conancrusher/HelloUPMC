0f5412d5a664afaab8bd0ed97f0c687dd600e676
ackermans <ackermans@192.168.56.1>
2017-02-03 14:35:24 -0500

    Added second println

--- HelloUPMC/src/HelloUPMC.java
+++ HelloUPMC/src/HelloUPMC.java
@@ -4,6 +4,7 @@ public class HelloUPMC {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello UPMC!");
+System.out.println("Life Changing Medicne");
 	}
 	
 
